5d65f60d7d919f4c0ce13440b71777658de21f1e
James Y <jyab0617@gmail.com>
2024-05-08 21:10:23 -0400

    reformat

--- Chip8.cpp
+++ Chip8.cpp
@@ -5,24 +5,21 @@
 #include "include/Chip8.h"
 #include <iostream>
 
-void Chip8::initialize()
-{
+void Chip8::initialize() {
     // initialize registers and memory once
     opcode = 0;
     I = 0;
     pc = 0x200;
     memory[pc] = 0xA2;
-    memory[pc+1] = 0xF0;
+    memory[pc + 1] = 0xF0;
 }
 
-void Chip8::emulateCycle()
-{
+void Chip8::emulateCycle() {
     // fetch opcode
     opcode = memory[pc] << 8 | memory[pc + 1];
 
     // decode opcode
-    switch (opcode & 0xF000)
-    {
+    switch (opcode & 0xF000) {
         case 0xA000:
             std::cout << "test A opcode" << std::endl;
             I = opcode & 0x0FFF;
