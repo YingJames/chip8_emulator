81c74a1d58bd91737c1712508d81f96073b8cda6
James Y <jyab0617@gmail.com>
2024-05-19 22:57:05 -0400

    add chip8 frequency

--- source/main.cpp
+++ source/main.cpp
@@ -30,10 +30,8 @@ int main() {
     }
 
     Chip8.loadROM("../output.ch8");
-
-    const double target_frequency = 60.0;
+    const double target_frequency = 1000.0;
     const std::chrono::duration<double, std::milli> target_delay(1000.0 / target_frequency);
-
     auto last_cycle_time = std::chrono::high_resolution_clock::now();
 
     SDL_Event event;
@@ -49,15 +47,14 @@ int main() {
 
             Chip8.emulateCycle();
 
-        auto current_time = std::chrono::high_resolution_clock::now();
-        auto elapsed_time = current_time - last_cycle_time;
-
-        if (elapsed_time < target_delay) {
-            // sleep for the remaining time if cycle too fast
-            std::this_thread::sleep_for(target_delay - elapsed_time);
+            auto current_time = std::chrono::high_resolution_clock::now();
+            auto elapsed_time = current_time - last_cycle_time;
+            if (elapsed_time < target_delay) {
+                // sleep for the remaining time if cycle too fast
+                std::this_thread::sleep_for(target_delay - elapsed_time);
+            }
+            last_cycle_time = std::chrono::high_resolution_clock::now();
         }
-
-        last_cycle_time = std::chrono::high_resolution_clock::now();
     }
     return 0;
 }
