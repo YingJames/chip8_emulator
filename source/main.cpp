4757f82729bfbc69f630b3d66a6f0931a516bb4e
James Y <jyab0617@gmail.com>
2024-05-19 22:56:41 -0400

    add SDL window init and listening

--- source/main.cpp
+++ source/main.cpp
@@ -2,13 +2,33 @@
 #include <ratio>
 #include <chrono>
 #include <thread>
+#include <SDL.h>
 #include "Chip8.h"
-
+#define WIDTH 800
+#define HEIGHT 600
 Chip8 Chip8;
 
 int main() {
 //    std::cout << "Hello, World!" << std::endl;
     Chip8.initialize();
+
+    // SDL INITIALIZATION
+    SDL_Window *window = nullptr;
+    if ((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1)) {
+        printf("Could not initialize SDL: %s.\n", SDL_GetError());
+        exit(-1);
+    }
+    window = SDL_CreateWindow("Chip-8 Emulator",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              WIDTH,
+                              HEIGHT,
+                              0);
+    if (window == nullptr) {
+        fprintf(stderr, "SDL window failed to initialise: %s\n", SDL_GetError());
+        return 1;
+    }
+
     Chip8.loadROM("../output.ch8");
 
     const double target_frequency = 60.0;
@@ -16,8 +36,18 @@ int main() {
 
     auto last_cycle_time = std::chrono::high_resolution_clock::now();
 
-    while (true) {
-        Chip8.emulateCycle();
+    SDL_Event event;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+//            else if (event.type == SDL_KEYDOWN) {
+//                Chip8.handleKeyDown(event.key.keysym.scancode);
+//            }
+
+            Chip8.emulateCycle();
 
         auto current_time = std::chrono::high_resolution_clock::now();
         auto elapsed_time = current_time - last_cycle_time;
